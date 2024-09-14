ff804aa5c5f4bfe05573f74a8a5bb96d5e5f721b
James <james.lyons@dematic.com>
2024-09-14 19:16:49 +0100

    convert to use prisma and master schema

--- src/processTracker.ts
+++ src/processTracker.ts
@@ -1,13 +1,11 @@
-//import the database
-import db from "./db/mysqlConnection.js";
-
 //import node-cron
 import cron from "node-cron";
+import db from "./db/db.js";
 
 let localProcessName = "";
 
 //when the process starts, log the process name and the start time
-function startProcess(processName: string) {
+async function startProcess(processName: string) {
   console.log(`${processName} started at ${new Date()}`);
 
   localProcessName = processName;
@@ -19,37 +17,56 @@ function startProcess(processName: string) {
   const mysqlStartTime = startTime.toISOString().slice(0, 19).replace("T", " ");
 
   //update the database if the processName is in the database already, otherwise insert it
-  const sql = `INSERT INTO processTracker (processName, lastRestart) VALUES ('${processName}', '${mysqlStartTime}') ON DUPLICATE KEY UPDATE lastRestart = '${mysqlStartTime}'`;
 
-  //console.log(sql);
-  //run the sql
-  db.query(sql, (err: any, result: { affectedRows: string }) => {
-    if (err) throw err;
-    //console.log("ProcessTracker: " + result.affectedRows + " record(s) updated");
+  const result = await db.dashboardSystemProcesses.findUnique({
+    where: {
+      processName: processName,
+    },
+  });
+
+  if (result !== null) {
+    //update the database
+    const result = await db.dashboardSystemProcesses.update({
+      where: {
+        processName: processName,
+      },
+      data: {
+        lastWatchdog: startTime,
+      },
+    });
+
+    return;
+  }
+
+  await db.dashboardSystemProcesses.create({
+    data: {
+      processName: processName,
+      lastWatchdog: startTime,
+    },
   });
 }
 
 //every 5seconds, update the database with the current time
-function updateProcess() {
+async function updateProcess() {
   //make timestamp for the start time
   const updateTime = new Date();
 
   //convert the start time to a mysql timestamp
   const mysqlUpdateTime = updateTime.toISOString().slice(0, 19).replace("T", " ");
 
-  //update the database if the processName is in the database already, otherwise insert it
-  const sql = `INSERT INTO processTracker (processName, lastWatchdog) VALUES ('${localProcessName}', '${mysqlUpdateTime}') ON DUPLICATE KEY UPDATE lastWatchdog = '${mysqlUpdateTime}'`;
-
-  //console.log(sql);
-  //run the sql
-  db.query(sql, (err: any, result: { affectedRows: string }) => {
-    if (err) throw err;
-    //console.log("ProcessTracker: " + result.affectedRows + " record(s) updated");
+  //update the database
+  const result = await db.dashboardSystemProcesses.update({
+    where: {
+      processName: localProcessName,
+    },
+    data: {
+      lastWatchdog: updateTime,
+    },
   });
 }
 
 //capture crash events
-function crashProcess(err: Error) {
+async function crashProcess(err: Error) {
   console.log(`${localProcessName} crashed at ${new Date()}`);
 
   //make timestamp for the crash time
@@ -62,22 +79,25 @@ function crashProcess(err: Error) {
   const crashReason = err.message;
   console.log("Crash reason: " + crashReason);
 
-  //update the database if the processName is in the database already, otherwise insert it, with the crash reason
-  const sql = `INSERT INTO processTracker (processName, lastCrash, lastCrashReason) VALUES ('${localProcessName}', '${mysqlCrashTime}', '${crashReason}') ON DUPLICATE KEY UPDATE lastCrash = '${mysqlCrashTime}', lastCrashReason = '${crashReason}'`;
-
-  console.log(sql);
-
-  //run the sql
-  db.query(sql, (err: any, result: { affectedRows: string }) => {
-
-
-
-    process.exit(1);
-    if (err) throw err;
-    //console.log("ProcessTracker: " + result.affectedRows + " record(s) updated");
-    //exit the process
-    process.exit(1);
+  //update the database
+  const result = await db.dashboardSystemProcesses.update({
+    where: {
+      processName: localProcessName,
+    },
+    data: {
+      lastCrash: crashTime,
+      crashCount: {
+        increment: 1,
+      },
+      lastCrashMessage: crashReason,
+    },
   });
+
+  process.exit(1);
+  if (err) throw err;
+  //console.log("ProcessTracker: " + result.affectedRows + " record(s) updated");
+  //exit the process
+  process.exit(1);
 }
 
 //every 5 seconds, update the process tracker
